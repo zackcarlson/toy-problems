042e4834905f3ed39c5799acb772939f5c38a2a0
zackcarlson <zackt.carlson@gmail.com>
2018-07-24 14:56:29 -0700

    Create characterFrequency js/test and index files

---
+++ src/characterFrequency/characterFrequency.test.js
@@ -0,0 +1,7 @@
+import { characterFrequency } from './characterFrequency.js';
+
+describe('characterFrequency', () => {
+  it('should be a function', () => {
+    expect(typeof(characterFrequency)).toBe('function');
+  });
+});
