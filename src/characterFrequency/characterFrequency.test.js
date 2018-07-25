5dace595b360bf79827b2654ee191ee5056081f5
zackcarlson <zackt.carlson@gmail.com>
2018-07-25 12:55:22 -0700

    Test that the output is an array

--- src/characterFrequency/characterFrequency.test.js
+++ src/characterFrequency/characterFrequency.test.js
@@ -1,12 +1,16 @@
 import { characterFrequency } from './characterFrequency.js';
 
 describe('characterFrequency', () => {
+  const input = 'mississippi';
+  const output = [['i', 4], ['s', 4], ['p', 2], ['m', 1]];
+
   it('should be a function', () => {
     expect(typeof(characterFrequency)).toBe('function');
   });
+  it('should be an instance of an array', () => {
+    expect(Array.isArray(characterFrequency(input))).toBe(true);
+  });
   it('should return nestArr sorted alpha (ascend) & charFreq (descend)', () => {
-    let input = 'mississippi';
-    let output = [['i', 4], ['s', 4], ['p', 2], ['m', 1]];
     expect(characterFrequency(input)).toEqual(output);
   });
 });
