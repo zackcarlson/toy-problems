e43dfbe50614ab5ebed66992ca82bd3743075025
zackcarlson <zackt.carlson@gmail.com>
2018-07-24 23:28:40 -0700

    Test for correct output

--- src/characterFrequency/characterFrequency.test.js
+++ src/characterFrequency/characterFrequency.test.js
@@ -4,4 +4,10 @@ describe('characterFrequency', () => {
   it('should be a function', () => {
     expect(typeof(characterFrequency)).toBe('function');
   });
+
+  it('should return nested array sorted alphabetically (ascending) and by character freq (descending)', () => {
+    let input = 'mississippi';
+    let output = [['i', 4], ['s', 4], ['p', 2], ['m', 1]];
+    expect(characterFrequency(input)).toEqual(output);
+  });
 });
