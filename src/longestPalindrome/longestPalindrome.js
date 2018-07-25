ccf383c4abd60c3382051e0054d4050f449e2c75
zackcarlson <zackt.carlson@gmail.com>
2018-07-25 11:55:42 -0700

    Expound input, output, constraints/complexities, edge cases

--- src/longestPalindrome/longestPalindrome.js
+++ src/longestPalindrome/longestPalindrome.js
@@ -4,4 +4,12 @@
 * palindrome is "a racecar a". Count whitespaces as valid characters. Other
 * palindromes in the above string include "dad", "ete", " dad " (including
 * whitespace on each side of dad).
+
+IOCE:
+
+I: string
+O: longest palindrome string
+C: account for whitespace; quadratic
+E: NA
+
 */
