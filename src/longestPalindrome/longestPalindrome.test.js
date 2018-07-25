63957a4bdf9382f8de6a8b5742ccd59b91c0af81
zackcarlson <zackt.carlson@gmail.com>
2018-07-25 13:00:53 -0700

    Test that a string is returned and that an empty string is returned

--- src/longestPalindrome/longestPalindrome.test.js
+++ src/longestPalindrome/longestPalindrome.test.js
@@ -4,9 +4,15 @@ describe('longestPalindrome', () => {
   it('should be a function', () => {
     expect(typeof(longestPalindrome)).toBe('function');
   });
+  it('should return a string', () => {
+    expect(typeof(longestPalindrome('abc'))).toBe('string');
+  });
   it('should return the longest palindrome', () => {
     let input = 'My dad is a racecar athlete';
     let output = 'a racecar a';
     expect(longestPalindrome(input)).toEqual(output);
+  });
+  it('should return an empty string when given empty string as input', () => {
+    expect(longestPalindrome('')).toEqual('');
   });
 });
