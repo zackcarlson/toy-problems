8cbd8892b4c6b2a8020e351adff005c1b9a3d16e
zackcarlson <zackt.carlson@gmail.com>
2018-07-25 12:14:33 -0700

    Modify test expectation name

--- src/characterFrequency/characterFrequency.test.js
+++ src/characterFrequency/characterFrequency.test.js
@@ -4,8 +4,7 @@ describe('characterFrequency', () => {
   it('should be a function', () => {
     expect(typeof(characterFrequency)).toBe('function');
   });
-
-  it('should return nested array sorted alphabetically (ascending) and by character freq (descending)', () => {
+  it('should return nestArr sorted alpha (ascend) & charFreq (descend)', () => {
     let input = 'mississippi';
     let output = [['i', 4], ['s', 4], ['p', 2], ['m', 1]];
     expect(characterFrequency(input)).toEqual(output);
