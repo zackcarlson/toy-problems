31b19f1b98acdd81e3897d5b4d02e6866bc90b8c
zackcarlson <zackt.carlson@gmail.com>
2018-07-28 11:51:01 -0700

    Pseudocode input, output, constraints, and edge cases and diagram problem

--- src/evenOccurrence/evenOccurrence.js
+++ src/evenOccurrence/evenOccurrence.js
@@ -8,6 +8,36 @@
  * example usage:
  * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); //  4
+ * 
+ * 
+ * I: array of items
+ * O: first item that reoccurs evenly
+ * C: linear
+ * E: none
+ * 
+ * Transformation: 
+ * 
+ * create an object to store item, and occurences
+ * {
+ *   value: 2(occurenece)
+ * }
+ * iterate through array and collect all values and occurences 
+ * into object
+ * 
+ * {
+ *   1: 1,
+ *   7: 1,
+ *   2: 1,
+ *   4: 2,
+ *   5: 1,
+ *   6: 2,
+ *   8: 1,
+ *   9: 1,
+ * }
+ * 
+ * iterate through object and check if occurrences are even 
+ * return that value 
+ * 
 */
 
 export const evenOccurrence = (arr) => {
