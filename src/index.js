c9db93ec82a6778ef4990149bde4362490187bba
zackcarlson <zackt.carlson@gmail.com>
2018-08-22 22:07:07 -0700

    Import new functions

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import './characterFrequency';
 import './longestPalindrome';
 import './evenOccurrence';
 import './insertionSort';
+import './keyboardRow';
+import './reverseInteger';
+import './romanToInt';
+import './longestCommonPrefix';
