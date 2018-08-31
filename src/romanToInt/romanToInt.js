98762d3d375b14012b6ff5f0044865af7de9fce8
zackcarlson <zackt.carlson@gmail.com>
2018-08-31 15:35:44 -0700

    Indent functions

--- src/romanToInt/romanToInt.js
+++ src/romanToInt/romanToInt.js
@@ -9,35 +9,35 @@ const singleRomanVals = {
 };
 
 const doubleRomanVals = {
-'IV': 4,
-'IX': 9,
-'XL': 40,
-'XC': 90,
-'CD': 400,
-'CM': 900,
+  'IV': 4,
+  'IX': 9,
+  'XL': 40,
+  'XC': 90,
+  'CD': 400,
+  'CM': 900,
 };
 
 export const romanToInt = function(s) {
-let sum = 0;
-let str = s.slice();
+  let sum = 0;
+  let str = s.slice();
 
-for (let i = 0; i < str.length; i++) {
-  let doubleRoman = str.substring(i, i+2);
-  let foundDouble = doubleRomanVals[doubleRoman];
-  if (doubleRoman && foundDouble) {
-    sum += foundDouble;
-    str = str.split('');
-    str.splice(i, 2, '_');
-    str = str.join('');
+  for (let i = 0; i < str.length; i++) {
+    let doubleRoman = str.substring(i, i+2);
+    let foundDouble = doubleRomanVals[doubleRoman];
+    if (doubleRoman && foundDouble) {
+      sum += foundDouble;
+      str = str.split('');
+      str.splice(i, 2, '_');
+      str = str.join('');
+    }
   }
-}
 
-str.split('').forEach(letter => {
-  let foundSingle = singleRomanVals[letter];
-  if (foundSingle) {
-    sum += foundSingle;
-  }
-});
+  str.split('').forEach(letter => {
+    let foundSingle = singleRomanVals[letter];
+    if (foundSingle) {
+      sum += foundSingle;
+    }
+  });
 
-return sum;
+  return sum;
 };
