0121eb80192af23ead5d745265b1fa68163d26ab
zackcarlson <zackt.carlson@gmail.com>
2018-09-28 11:36:55 -0700

    Refactor code for invalid test cases

--- src/anagram/anagram.js
+++ src/anagram/anagram.js
@@ -2,21 +2,28 @@ export const anagram = (str1, str2) => {
   // return false if str lengths are unequal
   if (str1.length !== str2.length) return false;
   // create an object for letter counts
-  let letterFreq = {};
+  let str1LetterFreq = {};
+  let str2LetterFreq = {};
   // loop length of str1
-  for (let i = 0; i < str1.length; i++) {
+  for (let letter of str1) {
     // if letter is new, add letter to object and set to 1
     // else increment letter by 1
-    letterFreq[str1[i]] = ++letterFreq[str1[i]] || 1;
-    letterFreq[str2[i]] = ++letterFreq[str2[i]] || 1;
+    str1LetterFreq[letter] = ++str1LetterFreq[letter] || 1;
   }
 
+  for (let letter of str2) {
+    str2LetterFreq[letter] = ++str2LetterFreq[letter] || 1;
+  }
+
+
   // loop through object 
-  for (let freq in letterFreq) {
-    // return false if letter count is unevenly divisible by 2
-    if (letterFreq[freq] % 2 !== 0) {
-      return false;
-    }
+  for (let freq in str1LetterFreq) {
+    // return false if letter of str1 is not present in str2 and
+    // letter's count of str1 is not the same
+    if (
+      !str2LetterFreq[freq] &&
+      str1LetterFreq[freq] !== str2LetterFreq[freq]
+    ) return false;
   }
   // return true 
   return true;
--- src/anagram/anagram.test.js
+++ src/anagram/anagram.test.js
@@ -1,8 +1,11 @@
 import { anagram } from './anagram.js';
 
 describe('anagram', () => {
-  const inputs = ['anagram', 'nagaram'];
+  const inputs = ['anagram', 'nagaram', 'aabb', 'sstt'];
   it('should return true if the second str is an anagram of first' , () => {
     expect(anagram(inputs[0], inputs[1])).toBe(true);
+  });
+  it('should return false if second str is not an anagram of first', () => {
+    expect(anagram(inputs[1], inputs[2])).toBe(false);
   });
 });
