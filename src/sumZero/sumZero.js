051d2e6f4a7cb3b709cc3e6f957aeee1556edd22
zackcarlson <zackt.carlson@gmail.com>
2018-09-28 19:45:39 -0700

    Refactor to faster time complexity

---
+++ src/sumZero/sumZero.js
@@ -0,0 +1,25 @@
+function sumZero(sortArrNums) {
+  // create left idx and right idx
+  let left = 0, right = sortArrNums.length - 1;
+  // while left is less than right 
+  while (left < right) {
+    let sum = sortArrNums[left] + sortArrNums[right];
+    // if sum of els at left & right = 0 
+    if (sum === 0) {
+      // return left and right as array
+      return [sortArrNums[left], sortArrNums[right]];
+      // if sum  is greater than 0 
+      // decrement right idx
+    } else if (sum > 0) {
+      right--;
+      // else increase left idx
+    } else {
+      left++;
+    }
+  }
+  // return undefined
+  return undefined;
+
+}
+const list = [-2, 0, 1, 2, 3];
+console.log(sumZero(list));
