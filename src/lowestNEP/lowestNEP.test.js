adffc5d970773a2298308e59a41b9c4817dc8a3c
zackcarlson <zackt.carlson@gmail.com>
2018-11-29 11:55:15 -0800

    Test for array with negative integers

--- src/lowestNEP/lowestNEP.test.js
+++ src/lowestNEP/lowestNEP.test.js
@@ -5,4 +5,9 @@ describe('lowestNEP', () => {
     expect(lowestNEP([3, 4, -1, 1])).toEqual(2);
     expect(lowestNEP([1, 2, 0])).toEqual(3);
   });
+
+  it('should return 1 when given an array of negative integers',  () => {
+    expect(lowestNEP([-3, -4, -1, -1])).toEqual(1);
+    expect(lowestNEP([-1, -2, -10])).toEqual(1);
+  });
 });
